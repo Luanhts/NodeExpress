a9e9ee26c0e5990439375371d3ca2dd57a9c278a
Luanhts <luanh3nrique08@gmail.com>
2024-08-13 20:59:29 -0300

    Mais rotas em Express

--- primeirosPassos/app.js
+++ primeirosPassos/app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send("Salve opa");
 });
 
+app.get('/login', (req, res) => {
+    res.send("login");
+});
+
+app.get('/menu/:infos', (req, res) => {
+    req.params;
+    res.send(`A informação foi ${req.params.infos}`)
+    
+    
+    //res.send("SEU MENU");
+});
+
 app.listen(porta, () => {
     console.log(`Seu server iniciou na porta http://localhost:${porta}`)
 })
