2d08a2088e74ce8b19345bee82e70fef06b2e20a
Luan <132374218+Luanhts@users.noreply.github.com>
2024-08-15 17:47:44 -0300

    Ex lions

--- ex2/app.js
+++ ex2/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const funcs = require('./controllers/operacoes');
 const porta = 3000;
 
 
@@ -7,16 +8,35 @@ app.get('/', (req, res) => {
     res.send("BEm vindo a calculadora: ");
 })
 
-app.get('/add', (req, res) => {
+app.get('/add/:num1/:num2', (req, res) => {
     req.params;
-    res.send("BEm vindo a calculadora: ");
+    res.send(`O resultado da soma é: `, funcs(params.num1, params.num2));
 })
 
-app.get('/sub', (req, res) => {
-    req.params;
-    res.send("BEm vindo a calculadora: ");
+/*
+app.use(express.json())
+
+// app.get('/calculadora/:n1/:n2', (req, res) => {
+//     const n1 = parseFloat(req.params.n1);
+//     const n2 = parseFloat(req.params.n2);
+//     const soma = n1 + n2;
+    
+//     // res.send(req.params);
+//     res.send(`A soma é: ${soma}`); 
+//     // res.json(req.params)
+// });
+
+app.get('/calculadora/somar', (req, res) => {
+
+    const result = op.soma(req.body)
+
+    res.send(`A soma é: ${result}`);
 })
+
+*/
 
 app.listen(porta, () => {
     console.log(`Seu servidor iniciou em http://localhost:${porta}`);
 })
+
+
